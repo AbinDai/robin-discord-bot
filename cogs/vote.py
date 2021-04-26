a2739ee8d1cd4782f6a14267c43fe0f3a760fe05
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-04-26 13:45:54 +0800

    Update vote.py

--- cogs/vote.py
+++ cogs/vote.py
@@ -9,7 +9,7 @@ class Vote(commands.Cog):
   async def upvote(self, ctx):
     embed = discord.Embed(
       title = "Vote Robin untuk Membantu Bot Ini Berkembang!",
-      description = "Apa kamu suka dengan fitur-fitur yang ada pada bot ini? Jangan lupa berikan dukunganmu dengan cara nge-Vote saya di top.gg. Vote-nya gratis, tidak dipungut biaya!\n\Vote [disini](https://top.gg/bot/805876219647361053/vote)!",
+      description = "Apa kamu suka dengan fitur-fitur yang ada pada bot ini? Jangan lupa berikan dukunganmu dengan cara nge-Vote saya di top.gg. Vote-nya gratis, tidak dipungut biaya!\n\nVote [disini](https://top.gg/bot/805876219647361053/vote)!",
       color = ctx.guild.get_member(805876219647361053).color
     )
     embed.set_thumbnail(url=self.client.user.avatar_url_as(format="png",size=4096))

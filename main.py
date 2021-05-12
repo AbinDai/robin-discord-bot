9b1f6daca5447c5853805ec103daef1c344a2ee9
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-05-12 17:11:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,10 @@ for filename in os.listdir('./cogs'):
         
 #isi status
 status = cycle([
-    'r!help'
+    'r!help',
+    'Selamat Hari Raya Idul Fitri!',
+    'Minal Aidin Wal Faizin',
+    'Mohon Maaf Lahir Batin'
 ])
 
 #===============================================================================================

4f3782e03a4a91f8f90c3b448fe2178d436c032e
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-05-13 22:23:09 +0800

    Update top-gg.py

--- cogs/top-gg.py
+++ cogs/top-gg.py
@@ -1,3 +1,4 @@
+import os
 from discord.ext import commands
 
 import dbl
@@ -6,7 +7,7 @@ import dbl
 class TopGG(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgwNTg3NjIxOTY0NzM2MTA1MyIsImJvdCI6dHJ1ZSwiaWF0IjoxNjIwOTE0ODE2fQ.duPaGHNn2W9X6E2Or-u2kN_MIWyaIxU9dp1jFqUfEw0'  
+        self.token = os.environ["TOPGG_TOKEN"]  
         self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True)  
 
     @commands.Cog.listener()

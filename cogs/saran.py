fb557f426ac9112c886a36b454c1ccf5231c0e93
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-05-29 21:59:03 +0800

    Update saran.py

--- cogs/saran.py
+++ cogs/saran.py
@@ -11,17 +11,18 @@ class Saran(commands.Cog):
 
     @commands.command(aliases=["feedback"])
     @commands.cooldown(1, 600, commands.BucketType.user)
-    async def saran(self, ctx, *, saran):
-        await ctx.reply(":white_check_mark: **Masukan Anda sudah diterima!** Terima kasih banyak atas kontribusinya :))")
+    async def saran(self, ctx, *, saran:str):
+        await ctx.reply(":white_check_mark: **Masukan Anda sudah terkirim!** Terima kasih banyak atas kontribusinya :))")
 
         channel = self.client.get_channel(824308102367019068)
         embed = discord.Embed(
-            description = f"```\n{saran}\n```",
+            title = "Ada Saran!"
+            description = saran,
             color = 0x058cfc
         )
         embed.set_footer(text=f"Dari Server: {ctx.author.guild}", icon_url=ctx.author.guild.icon_url)
-        embed.set_author(name=f"Ada Saran dari {ctx.author}:", icon_url=ctx.author.avatar_url)
-        await channel.send("<@550953412489904129> woy", embed=embed)
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        await channel.send(embed=embed)
 
 def setup(client):
     client.add_cog(Saran(client))

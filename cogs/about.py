0f77782bdf3f273837c4649b9fe547dfea82ddea
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-06-15 22:03:10 +0800

    Update about.py

--- cogs/about.py
+++ cogs/about.py
@@ -23,22 +23,52 @@ class About(commands.Cog):
         embed.add_field(name="Pengembang", value="Abin#4405")
         embed.add_field(name="Dibuat", value=self.client.user.created_at.strftime("%d/%m/%Y %H:%M:%S"))
 
-        embed.add_field(name="Jumlah Server", value=len(self.client.guilds))
-        embed.add_field(name="Jumlah User", value=len(self.client.users))
+        test = str(len(self.client.guilds))
+        jumlah_server = ''
+        while test:
+            jumlah_server += test[:3]
+            if len(test) > 3:
+                jumlah_server += '.'
+            test = test[3:]
+        embed.add_field(name="Jumlah Server", value=jumlah_server)
+        #
+        test = str(len(self.client.users))
+        jumlah_pengguna = ''
+        while test:
+            jumlah_pengguna += test[:3]
+            if len(test) > 3:
+                jumlah_pengguna += '.'
+            test = test[3:]
+        embed.add_field(name="Jumlah User", value=jumlah_pengguna)
         #
         list_text_channel = []
         for guild in self.client.guilds:
             for channel in guild.text_channels:
                 list_text_channel.append(channel)
-        embed.add_field(name="Jumlah Channel", value=len(list_text_channel))
+        test = str(len(list_text_channel))
+        jumlah_text_channel = ''
+        while test:
+            jumlah_text_channel += test[:3]
+            if len(test) > 3:
+                jumlah_text_channel += '.'
+            test = test[3:]
+        embed.add_field(name="Jumlah Channel", value=jumlah_text_channel)
         list_text_channel.clear()
 
         embed.add_field(name="Library", value=f"discord.py v{discord.__version__}")
         embed.add_field(name="Bahasa Program", value=f"Python v{platform.python_version()}")
         embed.add_field(name="Ping Bot", value=f"{round(self.client.latency*1000)}ms")
 
-        embed.add_field(name="Uptime", value=str(datetime.timedelta(seconds=int(round(time.time()-startTime)))))
-        embed.add_field(name="Jumlah Emoji", value=len(self.client.emojis))
+        embed.add_field(name="Waktu Aktif", value=str(datetime.timedelta(seconds=int(round(time.time()-startTime)))))
+        #
+        test = str(len(self.client.emojis))
+        jumlah_emoji = ''
+        while test:
+            jumlah_emoji += test[:3]
+            if len(test) > 3:
+                jumlah_emoji += '.'
+            test = test[3:]
+        embed.add_field(name="Jumlah Emoji", value=jumlah_emoji)
         embed.add_field(name="ID Bot", value=self.client.user.id)
 
         embed.add_field(

46df8cf99cc6da414c0834361cb16e7ccd113787
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-06-15 11:50:15 +0800

    Update about.py

--- cogs/about.py
+++ cogs/about.py
@@ -1,4 +1,4 @@
-import discord, time, datetime, platform, random
+import discord, platform, datetime, time
 from discord.ext import commands
 
 class About(commands.Cog):
@@ -7,42 +7,49 @@ class About(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("about.py siap")
+        global startTime
+        startTime = time.time()
 
-    @commands.command(aliases=['tentangbot', 'aboutme', 'aboutbot', 'tentang', 'info', 'infobot'])
-    @commands.cooldown(1, 5, commands.BucketType.user)
+    @commands.command(aliases=['tentangbot', 'aboutme', 'aboutbot', 'tentang', 'info', 'infobot', 'botinfo'])
     async def about(self, ctx):
-        deftar_foto_random = [
-            f"{self.client.user.avatar_url_as(format='png',size=4096)}",
-            f"{self.client.user.avatar_url_as(format='png',size=4096)}",
-            f"{self.client.user.avatar_url_as(format='png',size=4096)}",
-            f"{self.client.user.avatar_url_as(format='png',size=4096)}",
-            f"{self.client.user.avatar_url_as(format='png',size=4096)}",
-            "https://media1.tenor.com/images/8c409e6f39acc1bd796e8031747f19ad/tenor.gif?itemid=17029825"
-        ]
-        
         embed = discord.Embed(
-            title = 'Tentang Bot',
-            colour = ctx.guild.get_member(self.client.user.id).color
+            title = "Informasi Bot",
+            color = ctx.guild.me.color
         )
-        current_time = time.time()
-        difference = int(round(current_time))
-        text = str(datetime.timedelta(seconds=difference))
-        embed.set_thumbnail(url=random.choice(deftar_foto_random))
-        embed.set_footer(text=f'Di-Request oleh {ctx.author}', icon_url=ctx.author.avatar_url)
-        embed.add_field(name='Nama Bot:', value=self.client.user)
-        embed.add_field(name='Dibuat:', value='27 Februari 2021')
-        embed.add_field(name='Pengembang:', value='Abin#4405')
-        embed.add_field(name='Library:', value='discord.py')
-        embed.add_field(name='Bahasa Pemrograman:', value='Python')
-        embed.add_field(name='Ping Bot:', value=f'{round(self.client.latency * 1000)}ms')
-        embed.add_field(name='Versi Library:', value=f'v{discord.__version__}')
-        embed.add_field(name='Versi Python:', value=f'v{platform.python_version()}')
-        embed.add_field(name='Jumlah Server:', value=len(self.client.guilds))
-        embed.add_field(name="⠀", value="[Link Invite](https://top.gg/bot/805876219647361053/invite)\n"
-                                        "[Vote Disini!](https://top.gg/bot/805876219647361053/vote)\n"
-                                        "[Repositori GitHub](https://github.com/AbinDai/robin-discord-bot/)\n"
-                                        "[Help Server](https://discord.gg/rZqsvrMdwR)", inline=False)
+        embed.set_author(name=self.client.user, icon_url=self.client.user.avatar_url)
+        embed.set_thumbnail(url=self.client.user.avatar_url_as(format="png", size=4096))
+
+        embed.add_field(name="Nama", value=f"[{self.client.user.name}](https://onepiece.fandom.com/id/wiki/Nico_Robin)")
+        embed.add_field(name="Pengembang", value="Abin#4405")
+        embed.add_field(name="Dibuat", value=self.client.user.created_at.strftime("%d/%m/%Y %H:%M:%S"))
+
+        embed.add_field(name="Jumlah Server", value=len(self.client.guilds))
+        embed.add_field(name="Jumlah User", value=len(self.client.users))
+        #
+        list_text_channel = []
+        for guild in self.client.guilds:
+            for channel in guild.text_channels:
+                list_text_channel.append(channel)
+        embed.add_field(name="Jumlah Channel", value=len(list_text_channel))
+        list_text_channel.clear()
+
+        embed.add_field(name="Library", value=f"discord.py v{discord.__version__}")
+        embed.add_field(name="Bahasa Program", value=f"Python v{platform.python_version()}")
+        embed.add_field(name="Ping Bot", value=f"{round(self.client.latency*1000)}ms")
+
+        embed.add_field(name="Uptime", value=str(datetime.timedelta(seconds=int(round(time.time()-startTime)))))
+        embed.add_field(name="Jumlah Emoji", value=len(self.client.emojis))
+        embed.add_field(name="ID Bot", value=self.client.user.id)
+
+        embed.add_field(
+            name="⠀",
+            value="[Link Invite](https://top.gg/bot/805876219647361053/invite) |\
+                   [Vote Disini!](https://top.gg/bot/805876219647361053/vote) |\
+                   [Repositori GitHub](https://github.com/AbinDai/robin-discord-bot/) |\
+                   [Help Server](https://discord.gg/rZqsvrMdwR)",
+            inline=False
+        )
+
         await ctx.send(embed=embed)
 
 def setup(client):

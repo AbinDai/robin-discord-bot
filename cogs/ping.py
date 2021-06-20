9809d242698d46430da1acc293c7590c17550229
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-06-21 06:32:12 +0800

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,58 +1,49 @@
-import discord, asyncio
+import discord, time
 from discord.ext import commands
+from discord.ext.commands.context import Context
+from discord.ext.commands.cooldowns import BucketType
 
 class Ping(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("ping.py siap")
-    
-    @commands.command(aliases=["pong", "ponggo", "latensi"])
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    async def ping(self, ctx):
+    @commands.command(aliases=["pong","ponggo","latensi"])
+    @commands.cooldown(1, 5, type=BucketType.user)
+    async def ping(self, ctx:Context):
         ping = round(self.client.latency*1000)
 
-        if ping <= 299:
+        def embed():
             embed = discord.Embed(
-                color = discord.Color.default()
+                title = "<a:loading:854013569552220200> Menghitung Ping...",
+                color = discord.Color.blurple()
             )
-            embed.set_author(name="Menghitung Ping...", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif")
-            pesan_awal = await ctx.send(embed=embed)
+            return embed
 
+        def embed2(warna:str):
             embed = discord.Embed(
-                title = f":signal_strength: Ping Bot: `{ping}`ms",
-                color = 0x22ff00
+                title = f"📶 Ping: `{ping}`ms",
+                color = warna
             )
-            await asyncio.sleep(1.5)
-            await pesan_awal.edit(embed=embed)
-        elif ping >= 300 and ping < 399:
-            embed = discord.Embed(
-                color = discord.Color.default()
-            )
-            embed.set_author(name="Menghitung Ping...", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif")
-            pesan_avval = await ctx.send(embed=embed)
+            return embed
 
-            embed = discord.Embed(
-                title = f":signal_strength: Ping Bot: `{ping}`ms",
-                color = 0xffff00
-            )
-            await asyncio.sleep(1.5)
-            await pesan_avval.edit(embed=embed)
+        if ping < 300:
+            pesan_awal = await ctx.send(embed=embed())
+
+            time.sleep(2.5)
+
+            await pesan_awal.edit(embed=embed2(warna=0x00ff00))
+        elif ping >= 300 and ping < 400:
+            pesan_awal = await ctx.send(embed=embed())
+
+            time.sleep(2.5)
+
+            await pesan_awal.edit(embed=embed2(warna=0xffff00))
         elif ping >= 400:
-            embed = discord.Embed(
-                color = discord.Color.default()
-            )
-            embed.set_author(name="Menghitung Ping...", icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif")
-            pesan_aual = await ctx.send(embed=embed)
+            pesan_awal = await ctx.send(embed=embed())
 
-            embed = discord.Embed(
-                title = f":signal_strength: Ping Bot: `{ping}`ms",
-                color = 0xff0000
-            )
-            await asyncio.sleep(1.5)
-            await pesan_aual.edit(embed=embed)
+            time.sleep(2.5)
+
+            await pesan_awal.edit(embed=embed2(warna=0xff0000))
 
 def setup(client):
     client.add_cog(Ping(client))

1d8a4273261d7a283c34c884bf3571bc3f525f14
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-06-29 07:56:38 +0800

    Update bahasa-i.py

--- cogs/bahasa-i.py
+++ cogs/bahasa-i.py
@@ -7,7 +7,10 @@ class Bahasa_I(commands.Cog):
         self.client = client
 
     @commands.command(name="bahasa-i")
-    async def bhsaI(self, ctx:Context, *, teks:str):
+    async def bhsaI(self, ctx:Context, *, teks:str=None):
+        if not teks:
+            return await ctx.send("<:robin_palato:818892964457349220> **Sintaks tidak valid!** Masukkan teks!\nContoh: `r!bahasa-i ini adalah tes`")
+        
         await ctx.reply(content=teks.replace("a", "i").replace("u", "i").replace("e", "i").replace("o", "i"), mention_author=False)
 
 def setup(client):

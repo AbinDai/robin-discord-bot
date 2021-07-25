4c8097708dc357e6a95cc218c551b04746b692ee
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-07-25 23:02:32 +0800

    Create prefix-on-mention.py

---
+++ cogs/prefix-on-mention.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class PrefixonMention(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content in [f"<@!{self.client.user.id}>", f"<@{self.client.user.id}>"]:
+            embed = discord.Embed(description="Prefix: `r!`", color=discord.Color.random())
+            await message.channel.send(embed=embed)
+            
+def setup(client):
+    client.add_cog(PrefixonMention(client))

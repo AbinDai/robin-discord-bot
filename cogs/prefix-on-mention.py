eec7ab8b41259bdc743243365befcc5dcbc06d58
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-07-27 18:25:02 +0800

    Update prefix-on-mention.py

--- cogs/prefix-on-mention.py
+++ cogs/prefix-on-mention.py
@@ -8,7 +8,7 @@ class PrefixonMention(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.content in [f"<@!{self.client.user.id}>", f"<@{self.client.user.id}>"]:
-            embed = discord.Embed(description="Prefix: `r!`", color=discord.Color.random())
+            embed = discord.Embed(description="Prefix: `r!`", color=message.guild.me.color)
             await message.channel.send(embed=embed)
             
 def setup(client):

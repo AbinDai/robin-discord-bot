3a0c617615a29c31cc60a308da3e510514cad1b9
Abin:) <78573710+AbinDai@users.noreply.github.com>
2021-07-31 12:02:16 +0800

    Update gempa.py

--- cogs/gempa.py
+++ cogs/gempa.py
@@ -1,40 +1,39 @@
-import discord
-import requests
+import discord, requests
 from discord.ext import commands
 
 class Gempa(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("gempa.py siap")
-
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def gempa(self, ctx):
-        api = requests.get("https://gempa-api-zhirrr.vercel.app/api/gempa").json()
-        waktu = api["Waktu"]
-        lintang = api["Lintang"]
-        bujur = api["Bujur"]
-        magnitudo = api["Magnitudo"]
-        kedalaman = api["Kedalaman"]
-        wilayah = api["Wilayah"]
-        map = api["Map"]
-
-        tanagoyang = discord.Embed(
-            title = "Gempa Bumi Terkini di Indonesia",
+        api = requests.get("https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json").json()
+        
+        embed = discord.Embed(
+            title = "Info Gempa Terkini di Indonesia",
             color = 0xff0000
         )
-        tanagoyang.set_thumbnail(url="http://iconsetc.com/icons-watermarks/simple-red/ocha/ocha_disaster-earthquake/ocha_disaster-earthquake_simple-red_512x512.png")
-        tanagoyang.set_footer(text=f"Di-Request oleh {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
-        tanagoyang.add_field(name="Wilayah", value=wilayah, inline=False)
-        tanagoyang.add_field(name="Waktu", value=waktu, inline=False)
-        tanagoyang.add_field(name="Kedalaman", value=kedalaman, inline=False)
-        tanagoyang.add_field(name="Lintang", value=lintang, inline=False)
-        tanagoyang.add_field(name="Bujur", value=bujur, inline=False)
-        tanagoyang.add_field(name="Magnitudo", value=f"{magnitudo}\n\nJangan panik, tetap pantau keadaan terkini, dan waspada terhadap gempa susulan yang mungkin bisa saja terjadi.", inline=False)
-        await ctx.send(embed=tanagoyang)    
-
+        embed.set_thumbnail(url="http://iconsetc.com/icons-watermarks/simple-red/ocha/ocha_disaster-earthquake/ocha_disaster-earthquake_simple-red_512x512.png")
+        embed.set_image(url=api["Infogempa"]["gempa"]["Shakemap"])
+        embed.set_footer(text="Badan Meteorologi, Klimatologi, dan Geofisika", icon_url="https://cdn.bmkg.go.id/Web/Logo-BMKG-new-242x300.png")
+        
+        embed.add_field(name="Tanggal", value=api["Infogempa"]["gempa"]["Tanggal"])
+        embed.add_field(name="Jam", value=api["Infogempa"]["gempa"]["Jam"])
+        embed.add_field(name="Datetime", value=api["Infogempa"]["gempa"]["DateTime"])
+        
+        embed.add_field(name="Koordinat", value=api["Infogempa"]["gempa"]["Coordinates"])
+        embed.add_field(name="Lintang", value=api["Infogempa"]["gempa"]["Lintang"])
+        embed.add_field(name="Bujur", value=api["Infogempa"]["gempa"]["Bujur"])
+        
+        embed.add_field(name="Magnitudo", value=api["Infogempa"]["gempa"]["Magnitude"])
+        embed.add_field(name="Kedalaman", value=api["Infogempa"]["gempa"]["Kedalaman"])
+        embed.add_field(name="Wilayah", value=api["Infogempa"]["gempa"]["Wilayah"])
+        
+        embed.add_field(name="Potensi", value=api["Infogempa"]["gempa"]["Potensi"], inline=False)
+        embed.add_field(name="Dirasakan", value=api["Infogempa"]["gempa"]["Dirasakan"], inline=False)
+        
+        await ctx.send(embed=embed)
+        
 def setup(client):
     client.add_cog(Gempa(client))
